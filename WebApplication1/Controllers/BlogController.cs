5d74865e24bf260379f6f01e7b42c405168af093
stu <stu@qq.com>
2016-11-09 12:29:08 +0800

    使用cookie认证demo

--- WebApplication1/Controllers/BlogController.cs
+++ WebApplication1/Controllers/BlogController.cs
@@ -34,7 +34,14 @@
 
             public ActionResult AddArticle()
             {
-                return View();
+                if (Request.Cookies["isauth"] != null && Request.Cookies["isauth"].Value == "true")
+                                {
+                                    return View();
+                                }
+                            else
+            {
+                                    return RedirectToAction("login", "cookiedemo");
+                                }
             }
 
             public ActionResult ArticleSave(BlogArticle model)
